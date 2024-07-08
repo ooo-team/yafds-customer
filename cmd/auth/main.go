56cf4f42429452dd6850b4896f0778e3bf498414
dimoha_zadira <dimikson4@gmail.com>
2024-07-09 01:44:05 +0300

    architecture pics & compilable main

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
-	"fmt"
+	// "context"
+	"log"
+	// "github.com/ooo-team/YAFDS/internal/app"
 )
 
 func main() {
-	fmt.Println("auth web app")
+
+	// ctx := context.Background()
+	log.Println("kekes")
+	// a, err := app.NewApp(ctx)
+	// if err != nil {
+	// 	log.Fatalf("failed to init app: %s", err.Error())
+	// }
+
+	// err = a.Run()
+	// if err != nil {
+	// 	log.Fatalf("failed to run app: %s", err.Error())
+	// }
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// )
+
+// // init is invoked before main()
+// func init() {
+// 	// loads values from .env into the system
+// 	if err := godotenv.Load(); err != nil {
+// 		log.Print("No .env file found")
+// 	}
+// }
+
+// func load_dotenv_var(var_name string) string {
+// 	var_, exists := os.LookupEnv(var_name)
+
+// 	if !exists {
+// 		log.Panic("Dotenv variable {} does not exist", var_name)
+// 	}
+
+// 	return var_
+// }
+
+// func main() {
+// 	// Get the GITHUB_USERNAME environment variable
+// 	db_user := load_dotenv_var("db_user")
+// 	db_password := load_dotenv_var("db_password")
+
+// }
